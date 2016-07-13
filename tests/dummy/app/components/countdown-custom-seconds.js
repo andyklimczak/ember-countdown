d1f93d173683f18a076092f2d4acf703ed9478e1
Andy Klimczak <andyklimczak@fastmail.com>
2016-07-13 13:31:03 -0400

    Edit examples

--- tests/dummy/app/components/countdown-custom-seconds.js
+++ tests/dummy/app/components/countdown-custom-seconds.js
@@ -2,6 +2,5 @@ import countdown from 'countdownjs';
 import CountdownHTML from 'ember-countdown/components/countdown-html';
 
 export default CountdownHTML.extend({
-  htmlTag: 'b',
   units: countdown.SECONDS
 });
