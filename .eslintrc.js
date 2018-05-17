6dc49184a1c4878a91f86862b9416643a764f929
Ray Bradley <rbradley@infotechsoft.com>
2018-05-17 16:48:51 +0000

    Upgrade to Ember 2.18 LTS

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,49 @@ module.exports = {
     'plugin:ember/recommended'
   ],
   rules: {
-    'ember/no-old-shims': 'error'
+    'ember/no-old-shims': 'error',
   },
   env: {
     browser: true
   },
-  rules: {
-  }
+  overrides: [
+    // node files
+    {
+      files: [
+        'index.js',
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'app/**',
+        'addon/**',
+        'tests/dummy/app/**'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        'node/no-unpublished-require': ['error', {
+          'allowModules': ['broccoli-funnel', 'broccoli-merge-trees']
+        }]
+      })
+    },
+
+    // test files
+    {
+      files: ['tests/**/*.js'],
+      excludedFiles: ['tests/dummy/**/*.js'],
+      env: {
+        embertest: true
+      }
+    }
+  ]
 };
